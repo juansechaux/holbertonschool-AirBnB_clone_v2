351c28d3e19f2b0ef182cee917e0070b4b88bbcd
juansechaux <juansechaux@hotmail.com>
2023-12-03 12:53:51 -0500

    Task 5, Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
         if obj == None:
             return
         else:
