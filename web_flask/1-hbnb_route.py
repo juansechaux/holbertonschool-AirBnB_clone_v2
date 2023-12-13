fe1c85458428ae7d084b993564d818726276d21e
juansechaux <juansechaux@hotmail.com>
2023-12-13 18:20:53 -0500

    Task 1, script that starts a Flask web application, Routes: display Hello HBNB! hbnb: display HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,24 +3,20 @@
 from flask import Flask
 
 
-# Create an instance of the Flask application
 app = Flask(__name__)
 
 
-# Define the main route ("/") with strict_slashes=False option
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     '''Define the main route ("/") with strict_slashes=False option'''
     return "Hello HBNB!"
 
 
-# Define a new route ("/hbnb") with strict_slashes=False option
 @app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
+def hbnb():
     '''Define a new route ("/hbnb") with strict_slashes=False option'''
     return "HBNB"
 
 
-# Check if the script is being run as the main program
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
