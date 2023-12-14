ca9e7034556ba5eca9b9ca85bbdfa0df6ba06c99
juansechaux <juansechaux@hotmail.com>
2023-12-14 12:28:11 -0500

    Task 3, Variable Rules

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def c_text(text):
 
 
 @app.route("/python/<text>", strict_slashes=False)
-@app.route('/python/',strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 def python_text(text='is cool'):
     '''display “C ” followed by the value of the text variable'''
     new_text = text.replace('_', ' ')
